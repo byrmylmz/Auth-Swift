84820ed39c425c5481c10e6c1c4795d5800c1d5e
bayram <68708205+byrmylmz@users.noreply.github.com>
2022-06-14 17:50:49 +0300

    Initial Commit

---
+++ Auth-Swift/Auth_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Auth_SwiftApp.swift
+//  Auth-Swift
+//
+//  Created by Bayram Yilmaz on 14/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Auth_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Auth-Swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Auth-Swift
+//
+//  Created by Bayram Yilmaz on 14/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
